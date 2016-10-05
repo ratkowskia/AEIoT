a8868f59ca4dc0f16dcf170ca78b5fcf5e8be876
ar <ratkowskia@gmail.com>
2016-10-05 21:26:19 +0200

    algorithm search

--- mysite/settings.py
+++ mysite/settings.py
@@ -110,7 +110,7 @@ else:
             'NAME': 'djtesta',
             'USER': 'djtesta',
             'PASSWORD': 'qwerty45',
-            'HOST': '2001:4860:4864:1:36f2:838d:e3ad:e269',
+            'HOST': 'localhost',
             'PORT': '3306',
         }
     }
